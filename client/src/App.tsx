1c2359077841ecfb71f2e19c5b95c0a2f6c3f000
JoelCher <joelcher@hilma.tech>
2024-05-16 16:38:57 +0300

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+
+
+  useEffect(() => {
+    const canvas = document.querySelector();
+
+
+  }, [])
+  return (
+    <div className='app'>
+      <canvas className='game-canvas' />
+    </div>
+  )
+}
+
+export default App
